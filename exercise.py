61c99ed2d651563c2c9f2a2e39732f7dd5c7f0e2
christinegithub <christine13@gmail.com>
2019-03-27 18:24:49 -0400

    exercise complete

---
+++ exercise.py
@@ -0,0 +1,20 @@
+def longestConsecutive(list, k):
+    if len(list) == 0 | k > len(list) | k < 0:
+        return ""
+    else:
+        longestString = ""
+        for i in range(len(list)):
+            string = ""
+            if i + (k-1) < len(list):
+                for j in range(k):
+                    string += list[j + i]
+                if len(string) > len(longestString):
+                    longestString = string
+        return longestString
+
+
+a = longestConsecutive(["vengo", "ozzy", "absolutely", "little", "shark", "boo"], 2);
+print(a)
+
+s = longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3);
+print(s)
